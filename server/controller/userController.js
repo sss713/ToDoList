a372c8b5c496a15effeac46cdcd6bb3ec24f4f2b
sss713 <malikevich@gmail.com>
2023-10-13 19:36:57 +0300

    add registration logic | validation fixed

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,36 +1,32 @@
 import bcrypt from "bcryptjs";
-import {check, validationResult} from "express-validator";
 import db from "../db.js";
 
 class UserController {
-    async createUser(req, res){
+    async createUser(req, res){ 
         try {
 
-            const validationRules = [
-                check('email', "Uncorrect email").isEmail(),
-                check('password', "password must be longer than 8 and shorter than 36").isLength({min:8,max:36})
-            ];
-            
-            const errors = validationResult(req)
-    
-            if(!errors.isEmpty()) {
-                return res.status(400).json({message: "Uncorrect request", errors})
-            }
-            
-            // const candidate = req.bode
-            const {email, password} = req.body
-    
-            const candidate = await db.query('SELECT login FROM users where login = $1', [email])
-            console.log(candidate.rows)
-    
-            if (candidate.rows) {
-                return res.status(400).json({message: `User with email: ${email} already exist`})
+            const {login, password} = req.body
+
+            if (login.length > 3 && login.length < 20){
+
+                if (password.length > 8 && password.length < 36){
+
+                    const candidate = await db.query('SELECT login FROM users where login = $1', [login])
+                    if (candidate.rows[0] && candidate.rows[0].login === login) {
+                        return res.status(400).json({message: `User with login: ${login} already exist`})
+                    } else {
+                        const hashPassword = await bcrypt.hash(password, 7)
+                        const user = await db.query('INSERT INTO users (login, password) values ($1, $2) RETURNING *', [login, hashPassword])
+                        return res.json(user.rows);
+                    }
+
+                } else {
+                    return res.status(400).json({message: "password must be longer than 8 and shorter than 36"})
+                }
             } else {
-                const hashPassword = await bcrypt.hash(password, 7)
-                const user = await db.query('INSERT INTO users (login, password) values ($1, $2) RETURNING *', [email, hashPassword])
-                return res.json(user.rows);
-    
+                return res.status(400).json({message: "login must be longer than 3 and shorter than 20"})
             }
+           
         } catch (e) {
             console.log(e);
             return res.send({message:"Server error"})
