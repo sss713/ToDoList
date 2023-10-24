7ae7d34d481793057c1e9b29fc3526ce7a224052
sss713 <malikevich@gmail.com>
2023-10-24 17:12:27 +0300

    WIP | nedd add action

---
+++ client/src/components/registration/Login.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './authorization.css'
+import Input from "../../utils/input/Input";
+import {useDispatch} from "react-redux";
+import {login} from "../../actions/user";
+
+const Login = () => {
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+
+    return (
+        <div className='authorization'>
+            <div className="authorization__header">Авторизация</div>
+            <Input value={login} setValue={setLogin} type="text" placeholder="Введите email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
+            <button className="authorization__btn" onClick={() => dispatch(login(email, password))}>Войти</button>
+        </div>
+    );
+};
+
+export default Login;
--- client/src/components/registration/registration.jsx
+++ client/src/components/registration/registration.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import './registration.css'
-import Input from "../../utils/input/input";
+import React, {useState} from 'react';
+import './authorization.css'
+import Input from "../../utils/input/input.jsx";
+import {registration} from "../../actions/user";
 
 const Registration = () => {
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-        <div className="registration">
-            <div className="registration__header">Регистрация</div>
-            <Input type="text" placeholder=""/>
-            <Input type="text" placeholder=""/>
-            <Input type="text" placeholder=""/>
-            <button className="registration__btn">Регистрация</button>
+        <div className='authorization'>
+            <div className="authorization__header">Регистрация</div>
+            <Input value={login} setValue={setLogin} type="text" placeholder="Введите email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
+            <button className="authorization__btn" onClick={() => registration(email, password)}>Зарегистрироваться</button>
         </div>
-    )
-}
+    );
+};
 
 export default Registration;
