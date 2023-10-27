0fcc69adb084c17379af5f73f65fe200cc32cab7
terbik <terbik123255@gmail.com>
2023-10-27 04:08:56 +0300

    'finaly'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import json
 import logging
 import sys
 import time
@@ -9,6 +10,7 @@ from aiogram.fsm import state
 from aiogram import F
 from settings.config import dp, TOKEN
 from utils import StateGP
+from utils.API import Api_query
 from utils.StateGP import StateStatus
 from utils.model import Model, User, ToDoTask, ND
 from aiogram.fsm.context import FSMContext
@@ -27,186 +29,165 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 logging.basicConfig(level=logging.INFO)
 
 
-async def send_daily_task_notifications(user_chat_id):
+async def send_daily_task_notifications(user_chat_id, state):
     today = datetime.datetime.now().strftime('%Y-%m-%d')
     # formatted_date = datetime.datetime(2023, 12, 31)
     # today = formatted_date.strftime('%Y-%m-%d')
 
-    session = Model().session
-    query = session.query(ToDoTask.TDtask_name, ToDoTask.TDtask_description, ToDoTask.TDtask_deadline). \
-        join(ND, ToDoTask.TDtask_id == ND.TDtask_id). \
-        join(User, ND.user_id == User.user_id). \
-        filter(ToDoTask.TDtask_deadline == today). \
-        filter(User.telegram_id == user_chat_id).all()
-    if query:
-        task_lst = [f'{task[0]} - {task[1]} - {task[2]}' for task in query]
-        task_str = '\n'.join(task_lst)
-        message_text = f'Вам нужно выполнить следующие задачи на сегодня:\n{task_str}'
-        await bot.send_message(user_chat_id, message_text)
+    data = await state.get_data()
+    query = await Api_query.get_tasks_by_id(data)
+    query = json.loads(query)
+    task_list = []
+    if query.get("rows"):
+        for task in query["rows"]:
+            received_date = datetime.datetime.strptime(task["tdtask_deadline"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            current_date = datetime.datetime.now()
+            received_date += datetime.timedelta(hours=3)
 
-    session.close()
+            if received_date.date() == current_date.date():
+                task_list.append(f"{task.get('tdtask_name')} - {task.get('tdtask_description')} \n Важность: {task.get('tdtask_status')}")
+
+        task_str = '\n'.join(task_list)
+        if task_str != '':
+            message_text = f'Вам нужно выполнить следующие задачи на сегодня:\n{task_str}'
+            await bot.send_message(user_chat_id, message_text)
 
 
-async def background_task(chat_id):
+
+async def background_task(chat_id, state, data):
     user_chat_id = chat_id
     while True:
-        await send_daily_task_notifications(user_chat_id)
-        await asyncio.sleep(200)
+
+        await asyncio.sleep(15)
+        responses = await Api_query.turnoff_or_turn_alert_POST(data, chat_id)
+        responses = json.loads(responses)
+        if responses["user"]["enable_alerts"] is True:
+            await send_daily_task_notifications(user_chat_id, state)
 
 
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer('Этот бот создан для просмотра ваших задач и уведомления! \n '
                          'Чтобы войти вы должны быть зарегистрированы на сайте - https://savitto.ru/ \n'
-                         'После регистрации на сайте пишите /login\n'
-                         'Тестовые данные: \n'
-                         'Login: admin\n'
-                         'Password: 0000')
+                         'После регистрации на сайте пишите /login\n')
 
 @dp.message(Command("login"))
 async def start_to_login(message: Message, state: FSMContext):
-    await message.answer(f'{message.from_user.full_name}, введите логин: ')
+    await message.answer(f'{message.from_user.full_name}, введите логин(Логин должен быть в промежутке от 2 до 19 символов): ')
     await state.set_state(StateStatus.get_login)
 
+@dp.message(StateStatus.get_login)
+async def on_start(message: types.Message, state: FSMContext):
+    login_user = message.text
+    if 2 <= len(login_user) <= 19:
+        await message.answer('Введите пароль (Пароль должен быть в промежутке от 7 до 35 символов):')
+        await state.update_data(login=login_user)
+        await state.set_state(StateStatus.get_password)
+    else:
+        await message.answer('ваш пароль слишком длинный или слишком короткий')
+        await message.answer(f'{message.from_user.full_name}, введите логин: ')
+        await state.set_state(StateStatus.get_login)
 
-# @dp.message()
-# async def reject_provide(message: Message, state: FSMContext):
-#     state_status = await state.get_state()
-#     if state_status != StateStatus.profile:
-#         await message.answer('Вам нужно авторизоваться для использования функционала бота')
+@dp.message(StateStatus.get_password)
+async def get_password(message: types.Message, state: FSMContext):
+    password = message.text
+    if 7 <= len(password) <= 35:
+        data = await state.get_data()
+        data['password'] = password
+        response = await Api_query.get_id_user(data)
+        response = json.loads(response)
+        if "user" in response:
+            nickname = response["user"]["nickname"]
 
+            await message.answer(f'Вход успешен, {nickname}!')
+            await state.update_data(db_id_user=response["user"]["id"])
+            data["telegramid"] = message.from_user.id
+            await Api_query.post_id_telegram(data)
+
+            await send_daily_task_notifications(message.from_user.id, state)
+            await state.set_state(StateStatus.after_auth)
+            await menu(message, state)
+            await background_task(message.chat.id, state, data)
+
+        else:
+            await message.answer('Ошибка авторизации, неверный логин или пароль')
+            await message.answer('Введите логин еще раз:')
+            await state.set_state(StateStatus.get_login)
+    else:
+        await message.answer(f'Пароль должен быть в промежутке от 8 до 36 символов\nТекущая длина пароля {len(password)}')
 
 @dp.message(StateStatus.profile, lambda message: message.text == "Свои задачи")
 async def test(message: Message, state: FSMContext):
-    session = Model().session
-    # data = await state.get_data()
-    query = session.query(ToDoTask.TDtask_id, ToDoTask.TDtask_name, ToDoTask.TDtask_description,
-                          ToDoTask.TDtask_deadline). \
-        join(ND, ToDoTask.TDtask_id == ND.TDtask_id). \
-        join(User, ND.user_id == User.user_id). \
-        filter(User.Login == 'admin').all()
+    data = await state.get_data()
+    query = await Api_query.get_tasks_by_id(data)
+    response = json.loads(query)
+    if response.get("rows"):
+        for task in response["rows"]:
+            task_text = f"""{task.get('tdtask_id')}. {task.get('tdtask_name')} \n
+                            Описание: {task.get('tdtask_description')} \n
+                            Важность {task.get('tdtask_status')} \n
+                            Дедлайн: {task.get('tdtask_deadline').split('T')[0]}, 
+                            {task.get('tdtask_deadline').split('T')[1].split('.')[0]}"""
 
-    for task_object in query:
-        id_nickname = task_object[0]
-        select_text = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text='Удалить', callback_data=f'delete_task {task_object[0]}')]
-        ])
+            select_text = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text='Удалить', callback_data=f'delete_task {task["tdtask_id"]}')]
+            ])
 
-        await message.answer(f'{task_object[1]}', reply_markup=select_text)
+            await message.answer(task_text, reply_markup=select_text)
+    else:
+        await message.answer('Список задач пуст')
 
 
 @dp.callback_query(lambda cal: F.data == 'delete_task')
-async def some_test(callback: CallbackQuery):
-    session = Model().session
+async def some_test(callback: CallbackQuery, state: FSMContext):
     parametr = callback.data.split()
     id_task = parametr[1]
-    task_to_delete = session.query(ToDoTask).filter(ToDoTask.TDtask_id == id_task).first()
-    nd_to_delete = session.query(ND).filter(ND.TDtask_id == id_task).first()
-
-    if task_to_delete:
-        session.delete(task_to_delete)
-        session.delete(nd_to_delete)
-        session.commit()
-        await callback.answer('Удалено')
-        await callback.message.delete()
+    data = await state.get_data()
+    task_to_delete = await Api_query.delete_data_DELETE(data, id_task)
+    task_to_delete = json.loads(task_to_delete)
+    if 'rowCount' in task_to_delete:
+        if task_to_delete.get("rowCount") == 1:
+            await callback.answer('Удалено')
+            await callback.message.delete()
+        else:
+            await callback.answer('Ошибка нахождения задачи')
     else:
         await callback.answer('Ошибка удаления задачи')
 
 
-
-
-@dp.message(StateStatus.get_login)
-async def on_start(message: types.Message, state: FSMContext):
-    session = Model().session
-    login_user = message.text
-    await message.answer('Введите пароль')
-    await state.update_data(login_FSM=login_user)
-    await state.set_state(StateStatus.get_password)
-
-
-
-@dp.message(StateStatus.get_password)
-async def get_password(message: types.Message, state: FSMContext):
-    session = Model().session
-    password = message.text
-    data = await state.get_data()
-    true_profile = session.query(User.user_id).filter(User.Login == data['login_FSM']).filter(User.Password == password).first()
-
-    if true_profile:
-        nickname = session.query(User.Nickname).filter(User.Login == data['login_FSM']).first()
-
-        await message.answer(f'Вход успешен, {nickname[0]}!')
-
-        query_user = session.query(User).filter(User.Login == data['login_FSM']).first()
-
-        if query_user is not None:
-            query_user.telegram_id = message.chat.id
-            session.commit()
-
-        session.close()
-        # await send_daily_task_notifications(message.from_user.id)
-        # await send_notifications(message.from_user.id)
-        await state.set_state(StateStatus.after_auth)
-        await menu(message, state)
-        await background_task(message.chat.id)
-
-    else:
-        await message.answer('Ошибка авторизации, неверный логин или пароль')
-        await state.set_state(StateStatus.get_login)
-    session.close()
-
-
 @dp.message(StateStatus.after_auth)
 async def menu(message: Message, state: FSMContext):
+    # data = await state.get_data()
+    # response = Api_query.turnoff_or_turn_alert_POST(data, message.from_user.id)
+
     kb = [
         [KeyboardButton(text='Добавить задачу')],
         [KeyboardButton(text='Свои задачи')],
-        [KeyboardButton(text='Отказаться от уведомлений')],
-        [KeyboardButton(text='Включить уведомления')]
+        [KeyboardButton(text='Включить/Отключить уведомления')],
     ]
     keyboard = ReplyKeyboardMarkup(keyboard=kb)
     await message.answer('Выберите действие: ', reply_markup=keyboard)
     await state.set_state(StateStatus.profile)
 
 
-@dp.message(StateStatus.profile, lambda message: message.text == "Отказаться от уведомлений")
+@dp.message(StateStatus.profile, lambda message: message.text == "Включить/Отключить уведомления")
 async def remove_notifications(message: Message, state: FSMContext):
-    session = Model().session
     data = await state.get_data()
-    session.query(User).filter(User.Login == data['login_FSM']).update({'telegram_id': None})
-    session.commit()
-    session.close()
-    await message.answer('Уведомления отключены')
-    await state.set_state(StateStatus.after_auth)
-    await menu(message, state)
-
-
-@dp.message(StateStatus.profile, lambda message: message.text == "Включить уведомления")
-async def add_notifications(message: Message, state: FSMContext):
-    session = Model().session
-    data = await state.get_data()
-    session.query(User).filter(User.Login == data['login_FSM']).update({'telegram_id': message.chat.id})
-    session.commit()
-    session.close()
-    await message.answer('Уведомления включены')
-    await state.set_state(StateStatus.after_auth)
-    await menu(message, state)
-
-
-# @dp.message(StateStatus.profile, lambda message: message.text == "Свои задачи")
-# async def show_tasks(message: Message, state: FSMContext):
-#     session = Model().session
-#     data = await state.get_data()
-#     query = session.query(ToDoTask.TDtask_name, ToDoTask.TDtask_description, ToDoTask.TDtask_deadline). \
-#         join(ND, ToDoTask.TDtask_id == ND.TDtask_id). \
-#         join(User, ND.user_id == User.user_id). \
-#         filter(User.Login == data['login_FSM']).all()
-#     message_text = "Ваши задачи:\n"
-#     for idx, task in enumerate(query, start=1):
-#         task_name, task_description, task_deadline = task
-#         message_text += f"{idx}. Название: {task_name}\n   Описание: {task_description}\n   Дедлайн: {task_deadline}\n"
-#
-#     await message.answer(f'{message_text}')
+    responses = await Api_query.turnoff_or_turn_alert_POST(data, message.from_user.id)
+    responses = json.loads(responses)
+    if "message" not in responses:
+        if responses["user"]["enable_alerts"] == True:
+            await Api_query.turnoff_or_turn_alert_POST(data, message.from_user.id, enable_alert="false")
+            await message.answer('Уведомления отключены')
+            await state.set_state(StateStatus.after_auth)
+            await menu(message, state)
+        elif responses["user"]["enable_alerts"] == False:
+            await Api_query.turnoff_or_turn_alert_POST(data, message.from_user.id, enable_alert="true")
+            await message.answer('Уведомления включены')
+            await state.set_state(StateStatus.after_auth)
+            await menu(message, state)
+    else:
+        await message.answer('Ошибка отлючения уведомлений')
 
 
 @dp.message(StateStatus.profile, lambda message: message.text == "Добавить задачу")
@@ -221,12 +202,12 @@ async def add_task(message: Message, state: FSMContext):
 async def add_task_name_func(message: Message, state: FSMContext):
     task_name = message.text
     len_task_name = len(task_name)
-    if len_task_name <= 55:
+    if len_task_name <= 59:
         await state.update_data(TDtask_name=task_name)
         await message.answer('Задайте описание задачи: ')
         await state.set_state(StateStatus.add_task_description)
     else:
-        await message.answer(f'Привышен лимит строки, укажите менее 110 символов(текущий размер строки {len_task_name})')
+        await message.answer(f'Привышен лимит строки, укажите менее 60 символов(текущий размер строки {len_task_name})')
         await state.set_state(StateStatus.add_task_name)
 
 
@@ -234,12 +215,12 @@ async def add_task_name_func(message: Message, state: FSMContext):
 async def add_task_description_func(message: Message, state: FSMContext):
     task_description = message.text
     len_task_description = len(task_description)
-    if len_task_description <= 240:
+    if len_task_description <= 249:
         await state.update_data(TDtask_description=task_description)
         await message.answer('Задайте день дедлайна задачи(формат следующий - YYYY.MM.DD): ')
         await state.set_state(StateStatus.add_task_date)
     else:
-        await message.answer(f'Привышен лимит строки, укажите менее 240 символов(текущий размер строки {len_task_description})')
+        await message.answer(f'Привышен лимит строки, укажите менее 250 символов(текущий размер строки {len_task_description})')
         await state.set_state(StateStatus.add_task_description)
 
 
@@ -258,14 +239,12 @@ async def add_task_date_func(message: Message, state: FSMContext):
 @dp.message(StateStatus.add_task_status)
 async def add_task_status_func(message: Message, state: FSMContext):
     task_status = message.text
+    task_status = int(task_status)
+    await state.update_data(TDtask_status=task_status)
     data = await state.get_data()
-    id_user = message.from_user.id
     if 0 <= int(task_status) <= 10:
-        if await Model().add_task_to_db(id_user, int(task_status), data) == True:
-            await state.set_state(StateStatus.profile)
-        else:
-            await state.set_state(StateStatus.add_task_status)
-            await message.answer('Ошибка добавления')
+        response = await Api_query.add_task_POST(data, data["db_id_user"])
+        await message.answer("Успешно добавлена новая задача!")
     else:
         await state.set_state(StateStatus.add_task_status)
         await message.answer('Ошибка много взяли')
--- utils/API.py
+++ utils/API.py
@@ -1,24 +1,53 @@
-import aiohttp
 import json
-import asyncio
+
+import aiohttp
+
 
 class Api_query:
     @staticmethod
-    async def get_id_user(data):
+    async def request_1_POST(endpoint, data):
+        """здесь брать запросы где есть json файл"""
         async with aiohttp.ClientSession() as session:
             headers = {'Content-Type': 'application/json'}
-            async with session.post('http://127.0.0.1:5000/api/authorization', data=json.dumps(data), headers=headers) as response:
-                p = 2
+            async with session.post(f'http://localhost:5000/api/{endpoint}', data=json.dumps(data), headers=headers) as response:
                 return await response.text()
 
     @staticmethod
-    async def delete_data(data):
+    async def request_2_GET(endpoint):
+        """здесь нужно брать запросы те, в которых нету json файла"""
+        async with aiohttp.ClientSession() as session:
+            headers = {'Content-Type': 'application/json'}
 
+            async with session.get(f'http://localhost:5000/api/{endpoint}',
+                                   headers=headers) as response:
+                return await response.text()
+    @staticmethod
+    async def get_id_user(data):
+        return await Api_query.request_1_POST('authorization', data)
 
-async def main():
-    response = await Api_query.get_id_user({"login": "some", "password": "123456789"})
-    print(response)
+    @staticmethod
+    async def post_id_telegram(data):
+        return await Api_query.request_1_POST('tgauthorization', data)
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    @staticmethod
+    async def get_tasks_by_id(data):
+        return await Api_query.request_2_GET(f'posts/all?userId={data["db_id_user"]}')
+
+    @staticmethod
+    async def delete_data_DELETE(data, id_task):
+        async with aiohttp.ClientSession() as session:
+            headers = {'Content-Type': 'application/json'}
+            async with session.delete(f'http://localhost:5000/api/posts/del?taskId={id_task}&userId={data["db_id_user"]}', headers=headers) as response:
+                return await response.text()
+
+    @staticmethod
+    async def turnoff_or_turn_alert_POST(data, id_tg_user, enable_alert=""):
+        data["telegram_id"] = id_tg_user
+        data["enable_alerts"] = enable_alert
+        return await Api_query.request_1_POST(f'tgalerts', data)
+
+    @staticmethod
+    async def add_task_POST(data, id_user):
+        data["TDtask_completed"] = False
+        data["userId"] = id_user
+        return await Api_query.request_1_POST('posts', data)
